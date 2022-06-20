3243dcd7cbc42f1361ed1d12c30c9e3114ad3148
Almir Santos <almir.santos@gok.digital>
2022-06-20 13:29:05 -0300

    Text da dock

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -6,12 +6,12 @@ export default function Home() {
     <Section>
       <div className="BoxTop">
         <div className="BoxText">
-          <h1>Blog da Go-K</h1>
+          <h1>Blog da GO-K</h1>
           <br />
           <p>
-            Junte-se a milhares de devs e acelere na direção dos seus objetivos.
-            Transforme sua carreira em uma jornada de evolução contínua através
-            das melhores tecnologias.
+            Lorem ipsum hac posuere taciti iaculis curabitur ultrices primis,
+            lobortis sociosqu egestas rutrum taciti leo nisl quis leo, luctus
+            consequat lectus luctus risus at risus.
           </p>
         </div>
         <div className="BoxImageMouse">
